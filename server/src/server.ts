06c1dcf2b8836407a36a7280172206780b289cd6
Rishon Jaffe <rishonjaffe9@gmail.com>
2023-04-15 12:28:29 +0300

    404 route fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,11 +25,6 @@ app.get("/", (req, res) => {
   res.send("uwu");
 });
 
-// 404 route
-app.get("*", (req, res) => {
-  res.status(404).redirect("/");
-});
-
 // Define the endpoint for creating a new document
 app.post("/api/documents", async (req, res) => {
   const { content } = req.body;
@@ -66,6 +61,12 @@ app.get("/api/documents/:id", async (req, res) => {
     console.error(err);
     res.status(500).send({ message: "Server error" });
   }
+});
+
+// 404 route
+app.use((req, res, next) => {
+  res.status(404);
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
